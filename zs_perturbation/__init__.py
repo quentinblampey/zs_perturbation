ae8866e9793de146f9ed1814777fd4ed8eaedb0c
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2026-04-03 13:17:25 +0200

    start exploring

---
+++ zs_perturbation/__init__.py
@@ -0,0 +1,2 @@
+from .constants import DATASET_NAME, CONTROL, TO_GENE_SYMBOL, BENCHMARK_FILE
+from .download import download_dataset
---
+++ zs_perturbation/constants.py
@@ -0,0 +1,13 @@
+DATASET_NAME = "bulk-rna-immuno-inflammation-cohorts"
+BENCHMARK_FILE = "benchmark_drug_target_disease_matrix.csv"
+
+CONTROL = "Control"
+
+TO_GENE_SYMBOL = {
+    "BAFF": "TNFSF13B",
+    "CD20": "MS4A1",
+    "IgE": "IGHE",
+    "OX40": "TNFRSF4",
+    "PDE4": "PDE4A",
+    "TL1A": "TNFSF15",
+}
